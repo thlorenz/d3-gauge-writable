9188e4831dfd8ad172a0a5c919e9bd79b8c15020
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-26 22:47:00 -0400

    tests no longer overspecified

--- test/d3-gauge-writable.js
+++ test/d3-gauge-writable.js
@@ -20,17 +20,22 @@ test('\nwhen I pipe a stringified number stream into the gauge', function (t) {
   numbers({ to: 40, throttle: throttle }).pipe(w)
 
   t.notOk(g._currentRotation, 'initially it does not have a current rotation')
-  setTimeout(check.bind(null, -135    ) , wait  + 50     )
-  setTimeout(check.bind(null, -128.25 ) , 2 * wait + 50  )
-  setTimeout(check.bind(null, -121.5  ) , 3 * wait + 50  )
-  setTimeout(check.bind(null, -114.75 ) , 4 * wait + 50  )
-  setTimeout(check.bind(null, -108    ) , 5 * wait + 50  )
-  setTimeout(check.bind(null, -67.5   ) , 10 * wait + 50 )
-  setTimeout(check.bind(null, -33.75  ) , 15 * wait + 50 )
-  setTimeout(check.bind(null, 6.75    ) , 20 * wait + 50 )
-  setTimeout(check.bind(null, 135     ) , 40 * wait + 50 )
-  setTimeout(check.bind(null, 135     ) , 50 * wait + 50 )
-  setTimeout(t.end.bind(t) , 50 * wait + 100 )
+  var lastVal = -135;
+  setTimeout(function () { t.equal(g._currentRotation, -135, 'starts at -135 rotation') }, wait  + 50     )
+  setTimeout(check , 2 * wait + 50  )
+  setTimeout(check, 3 * wait + 50  )
+  setTimeout(check, 4 * wait + 50  )
+  setTimeout(check, 5 * wait + 50  )
+  setTimeout(check, 10 * wait + 50 )
+  setTimeout(check, 15 * wait + 50 )
+  setTimeout(check, 20 * wait + 50 )
+  setTimeout(check, 40 * wait + 50 )
+  setTimeout(function () { t.equal(g._currentRotation, 135, 'ends at 135 rotation') }, 55 * wait + 50 )
+  setTimeout(t.end.bind(t) , 55 * wait + 100 )
 
-  function check(val) { t.equal(g._currentRotation, val) }
+  function check() { 
+    var currentVal = g._currentRotation
+    t.ok(currentVal > lastVal, 'current: ' + currentVal + ' is greater than last: ' + lastVal) 
+    lastVal = currentVal 
+  }
 })
